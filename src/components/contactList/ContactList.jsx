f01f1efb7979afb00dc8c8ce05d6616921addfdc
Artem Sydorov <sydorovay@gmail.com>
2023-06-18 22:27:13 +0200

    change components to class

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,24 +1,29 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ContactList.module.css';
 
-const ContactList = ({ contacts, onDelete }) => {
-  return (
-    <ul className={styles['contact-list']}>
-      {contacts.map(contact => (
-        <li className={styles['contact-list-item']} key={contact.id}>
-          <span className={styles['contact-name']}>{contact.name}:</span>{' '}
-          {contact.number}
-          <button
-            className={styles['delete-button']}
-            onClick={() => onDelete(contact.id)}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+class ContactList extends Component {
+  render() {
+    const { contacts, onDelete } = this.props;
+
+    return (
+      <ul className={styles['contact-list']}>
+        {contacts.map(contact => (
+          <li className={styles['contact-list-item']} key={contact.id}>
+            <span className={styles['contact-name']}>{contact.name}:</span>{' '}
+            {contact.number}
+            <button
+              className={styles['delete-button']}
+              onClick={() => onDelete(contact.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,26 +1,31 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
 
- const Filter = ({ filter,  onChange}) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.subfilter}>
-        <span className={styles.label}>Filter contacts by name:</span>
-        <input
-          type="text"
-          value={filter}
-          onChange={onChange}
-          className={`${styles.input} ${styles.placeholder}`}
-          placeholder="Please enter name..."
-        />
+class Filter extends Component {
+  render() {
+    const { filter, onChange } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.subfilter}>
+          <span className={styles.label}>Filter contacts by name:</span>
+          <input
+            type="text"
+            value={filter}
+            onChange={onChange}
+            className={`${styles.input} ${styles.placeholder}`}
+            placeholder="Please enter name..."
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
-export default Filter
+export default Filter;
